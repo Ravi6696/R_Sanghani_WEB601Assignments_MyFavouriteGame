5be2bb68a188f86ee7fe07d36fae56cb3c084679
Ravi6696 <90625985+Ravi6696@users.noreply.github.com>
2022-02-20 22:19:09 -0500

    assignment3completed

---
+++ learning-angular/src/app/contentfilter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ContentfilterPipe } from './contentfilter.pipe';
+
+describe('ContentfilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new ContentfilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ learning-angular/src/app/contentfilter.pipe.ts
@@ -0,0 +1,21 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {Content} from "./helper-files/content-interface";
+
+@Pipe({
+  name: 'contentTypeFilter'
+})
+export class ContentfilterPipe implements PipeTransform {
+
+  transform(contentList: Content[], filterByThisType?: string): Content[] {
+    console.log("filter value: ", filterByThisType);
+    
+    return contentList.filter(c => {
+      if (filterByThisType) {
+        return c.type == filterByThisType;
+      } else { 
+        return !c.type; 
+      }
+    });
+  }
+
+}
