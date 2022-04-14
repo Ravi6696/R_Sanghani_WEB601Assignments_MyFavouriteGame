68c554277c37bfd8bb20287ef71263b9f82cc37e
Ravi6696 <90625985+Ravi6696@users.noreply.github.com>
2022-04-14 08:43:25 -0400

    assignment5

--- learning-angular/src/app/create-content/create-content.component.ts
+++ learning-angular/src/app/create-content/create-content.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Content } from '../helper-files/content-interface'
 
 @Component({
   selector: 'app-create-content',
   templateUrl: './create-content.component.html',
-  styleUrls: ['./create-content.component.scss']
+  styleUrls: ['./create-content.component.css']
 })
 export class CreateContentComponent implements OnInit {
+  @Output() newContentEvent = new EventEmitter<Content>();
+  newContent: Content = {
+    id: -1, title: "", description: '', creator: '', type: undefined
+  };
+  tempId: string = "";
+  tempTags: string = '';
+  errorMessage: string = '';
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  addContentFromChild(): void {
+
+    let gamePromise = new Promise((success, fail) => {
+      if (!this.tempId || !this.newContent.title || !this.newContent.description || !this.newContent.creator)
+      {
+        fail("something is missing");
+        return;
+      }
+      // part of the promise
+      console.log("child is working");
+      this.newContent.tags = this.tempTags.split(';');
+      this.newContent.id = parseInt(this.tempId);
+      this.newContentEvent.emit(this.newContent);
+      success("content added successfully " + this.newContent.id + ", title is: " + this.newContent.title);      
+    });
+    gamePromise.then((successMessage) => { 
+
+      console.log(successMessage);
+      this.newContent = {
+        id: -1, title: "", description: '', creator: '', type: undefined
+      };
+      this.tempId = "";
+      this.tempTags = ""
+      this.errorMessage = "";
+    }).catch(failMessage => {
+      this.errorMessage = "It failed terribly! - " + failMessage;
+    });
   }
 
 }
